e51fa679b0e4d374a7ea8196652cd221c0507866
Henry Steinberg <henry@quotapath.com>
2025-11-17 17:23:02 -0500

    adding in css as well as certain dev artifacts

---
+++ _data/metadata.js
@@ -0,0 +1,13 @@
+export default {
+	title: "Henry Steinberg",
+	url: "https://www.henrysteinberg.com/",
+	domain: "henrysteinberg.com",
+	language: "en",
+	description:
+		"This is the online home for Henry Steinberg showcasing his talents and capabilities.",
+	profile_image: "https://www.henrysteinberg.com/img/meta/",
+	author: {
+		name: "Henry Steinberg",
+		email: "henryjosephsteinberg@gmail.com",
+	},
+};
